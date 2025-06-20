84a33f4b41cd78a0d141f4e598972ce6f65b3122
Alessandro Visintin <alevise.public@gmail.com>
2025-06-20 07:46:59 +0200

    Updated AlarmReporting page

--- powershell/PSHttpServerApps/VirtCoe/Controllers/AlarmreportingController.ps1
+++ powershell/PSHttpServerApps/VirtCoe/Controllers/AlarmreportingController.ps1
@@ -1,4 +1,4 @@
-function Get-Alarmreporting {
+ function Get-Alarmreporting {
     param (
         [Parameter(Mandatory=$true)]
         [System.Net.HttpListenerRequest]$Request,
@@ -9,6 +9,7 @@ function Get-Alarmreporting {
 
     $Response.ContentType = "text/html"
 
+    $twoColumnView = Get-Content (Join-Path $script:ViewsPath "Utils/TwoColumnContainer.html") -Raw
     $layoutView = Get-Content (Join-Path $script:ViewsPath "Layout.html") -Raw
 
     $viewData = $global:TextContent.layout
@@ -19,62 +20,7 @@ function Get-Alarmreporting {
     $viewData.psobject.Properties |
         ForEach-Object { $layoutView = $layoutView -replace "{{$($_.Name)}}", $_.Value }
 
-    # $mainContent = ""
-    # $viewData.Services.GetEnumerator() |
-    # ForEach-Object { 
-    #     $serviceCard = $serviceCardView
-    #     $_.psobject.Properties |
-    #     ForEach-Object { $serviceCard = $serviceCard -replace "{{$($_.Name)}}", $_.Value }
-    #     $mainContent += $serviceCard
-    # }
-    # $indexView = $indexView -replace "{{MainContent}}", $mainContent 
+    $layoutView = $layoutView -replace "{{MainContent}}", $twoColumnView 
     return $layoutView
 
 }
-
-
-# function Get-VCenterEndpoints {
-#     param (
-#         [Parameter(Mandatory=$true)]
-#         [System.Net.HttpListenerRequest]$Request,
-        
-#         [Parameter(Mandatory=$true)]
-#         [System.Net.HttpListenerResponse]$Response
-#     )
-
-#     $reader = New-Object System.IO.StreamReader(
-#         $Request.InputStream,
-#         $Request.ContentEncoding
-#     )
-#     $requestBody = $reader.ReadToEnd()
-#     $reader.Close()
-
-#     $jsonRequest = $requestBody | ConvertFrom-Json
-
-
-#     $Response.ContentType = "application/json"
-    
-#     $orderReference = $searchData.orderReference
-#     try {
-#         $results = $global:CROCDatabase.getC3DSBVMWAREServers($orderReference)
-#         $responseObj = @{
-#             success = $true
-#             results = $results
-#             message = "Found $($results.Count) hosts for order reference: $orderReference"
-#         }
-
-#         $sessionId = Get-SessionId -Request $Request -Response $Response
-#         Set-SessionItem -SessionId $sessionId -Key "OrderReference" -Value $orderReference
-#         Set-SessionItem -SessionId $sessionId -Key $orderReference -Value $results
-
-#     }
-#     catch {
-#         $responseObj = @{
-#             success = $false
-#             results = @()
-#             message = "Error: $($_.Exception.Message)"
-#         }
-#     }
-    
-#     return ($responseObj | ConvertTo-Json)
-# }
